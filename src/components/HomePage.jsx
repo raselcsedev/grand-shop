7674cf9b976d69d403da693a907a702247a83439
raselcsedev <raselcse.dev@gmail.com>
2024-08-02 11:46:42 +0600

    landing page design

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -32,7 +32,16 @@ const HomePage = () => {
           </div>
           <h1>Title line h2</h1>
           <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras interdum augue sed tellus accumsan, nec congue quam lacinia. Duis ornare congue ante a facilisis. Vestibulum sed magna in ligula aliquam luctus non in ante. Nullam egestas, nunc at rutrum tincidunt, purus metus malesuada ante, at cursus massa turpis sit amet nulla. Quisque non ante quis lorem fermentum auctor sed sed ligula. Aenean turpis sapien, efficitur quis placerat at, facilisis bibendum erat. Aliquam quis mattis leo, sed egestas augue. Phasellus vitae vulputate nisi, vitae eleifend enim. Aenean rutrum tristique purus,
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
+            interdum augue sed tellus accumsan, nec congue quam lacinia. Duis
+            ornare congue ante a facilisis. Vestibulum sed magna in ligula
+            aliquam luctus non in ante. Nullam egestas, nunc at rutrum
+            tincidunt, purus metus malesuada ante, at cursus massa turpis sit
+            amet nulla. Quisque non ante quis lorem fermentum auctor sed sed
+            ligula. Aenean turpis sapien, efficitur quis placerat at, facilisis
+            bibendum erat. Aliquam quis mattis leo, sed egestas augue. Phasellus
+            vitae vulputate nisi, vitae eleifend enim. Aenean rutrum tristique
+            purus,
           </p>
           <button>
             Learn More{" "}
@@ -48,7 +57,16 @@ const HomePage = () => {
             Shop this is H2
           </h2>
           <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras interdum augue sed tellus accumsan, nec congue quam lacinia. Duis ornare congue ante a facilisis. Vestibulum sed magna in ligula aliquam luctus non in ante. Nullam egestas, nunc at rutrum tincidunt, purus metus malesuada ante, at cursus massa turpis sit amet nulla. Quisque non ante quis lorem fermentum auctor sed sed ligula. Aenean turpis sapien, efficitur quis placerat at, facilisis bibendum erat. Aliquam quis mattis leo, sed egestas augue. Phasellus vitae vulputate nisi, vitae eleifend enim. Aenean rutrum tristique purus,
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
+            interdum augue sed tellus accumsan, nec congue quam lacinia. Duis
+            ornare congue ante a facilisis. Vestibulum sed magna in ligula
+            aliquam luctus non in ante. Nullam egestas, nunc at rutrum
+            tincidunt, purus metus malesuada ante, at cursus massa turpis sit
+            amet nulla. Quisque non ante quis lorem fermentum auctor sed sed
+            ligula. Aenean turpis sapien, efficitur quis placerat at, facilisis
+            bibendum erat. Aliquam quis mattis leo, sed egestas augue. Phasellus
+            vitae vulputate nisi, vitae eleifend enim. Aenean rutrum tristique
+            purus,
           </p>
           <button>Learn more</button>
         </div>
@@ -58,12 +76,338 @@ const HomePage = () => {
             Shop this is H2
           </h2>
           <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras interdum augue sed tellus accumsan, nec congue quam lacinia. Duis ornare congue ante a facilisis. Vestibulum sed magna in ligula aliquam luctus non in ante. Nullam egestas, nunc at rutrum tincidunt, purus metus malesuada ante, at cursus massa turpis sit amet nulla. Quisque non ante quis lorem fermentum auctor sed sed ligula. Aenean turpis sapien, efficitur quis placerat at, facilisis bibendum erat. Aliquam quis mattis leo, sed egestas augue. Phasellus vitae vulputate nisi, vitae eleifend enim. Aenean rutrum tristique purus,
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
+            interdum augue sed tellus accumsan, nec congue quam lacinia. Duis
+            ornare congue ante a facilisis. Vestibulum sed magna in ligula
+            aliquam luctus non in ante. Nullam egestas, nunc at rutrum
+            tincidunt, purus metus malesuada ante, at cursus massa turpis sit
+            amet nulla. Quisque non ante quis lorem fermentum auctor sed sed
+            ligula. Aenean turpis sapien, efficitur quis placerat at, facilisis
+            bibendum erat. Aliquam quis mattis leo, sed egestas augue. Phasellus
+            vitae vulputate nisi, vitae eleifend enim. Aenean rutrum tristique
+            purus,
           </p>
           <button>Learn more </button>
         </div>
       </div>
-      
+      <div className="yellow-title-section">
+        <div className="yellow-title-bg"></div>
+        <img src="src\assets\images\hero-image.png" alt="...." />
+        <div className="title-line-details">
+          <h1>Yellow Title h2</h1>
+          <div>
+            <div className="yellow-title-icon"></div>
+          </div>
+          <h4>H3 Title</h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
+            interdum augue sed tellus accumsan, nec congue quam lacinia. Duis
+            ornare congue ante a facilisis. Vestibulum sed magna in ligula
+            aliquam luctus non in ante. Nullam egestas, nunc at rutrum
+            tincidunt, purus metus malesuada ante, at cursus massa turpis sit
+            amet nulla. Quisque non ante quis lorem fermentum auctor sed sed
+            ligula. Aenean turpis sapien, efficitur quis placerat at, facilisis
+            bibendum erat. Aliquam quis mattis leo, sed egestas augue. Phasellus
+            vitae vulputate nisi, vitae eleifend enim. Aenean rutrum tristique
+            purus,
+          </p>
+          <button>
+            Learn More{" "}
+            <img src="src\assets\images\arrow-right-white.png" alt="...." />{" "}
+          </button>
+        </div>
+      </div>
+
+      <div className="one-liner-section">
+        <h4>h2 title one liner</h4>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras interdum
+          augue sed tellus accumsan, nec congue quam lacinia. Duis ornare congue
+          ante a facilisis. Vestibulum sed magna in ligula aliquam luctus non in
+          ante. Nullam egestas, nunc at rutrum tincid
+        </p>
+      </div>
+
+      <div className="explore-section">
+        <div className="explore-details">
+          <h4>
+            <img src="src\assets\images\Group 3.png" alt="...." /> <span></span>{" "}
+            title h4
+          </h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
+            interdum augue sed tellus accumsan, nec congue quam lacinia. Duis
+            ornare congue ante a facilisis. Vestibulum sed magna in ligula
+            aliquam luctus non in ante. Nullam egestas, nunc at rutrum
+            tincidunt, purus metus malesuada ante, at cursus massa turpis sit
+            amet
+          </p>
+        </div>
+        <div className="explore-img pl">
+          <img src="src\assets\images\hero-image.png" alt="...." />
+          <div className="first-explore-card">
+            Explore <br /> Products
+          </div>
+        </div>
+      </div>
+      <div className="explore-section">
+        <div className="explore-img pr">
+          <img src="src\assets\images\hero-image.png" alt="...." />
+          <div className="second-explore-card">
+            Explore <br />
+            Cool <br /> Products
+          </div>
+        </div>
+        <div className="explore-details">
+          <h4>
+            <img src="src\assets\images\Frame.png" alt="...." /> <span></span>{" "}
+            title h4
+          </h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
+            interdum augue sed tellus accumsan, nec congue quam lacinia. Duis
+            ornare congue ante a facilisis. Vestibulum sed magna in ligula
+            aliquam luctus non in ante. Nullam egestas, nunc at rutrum
+            tincidunt, purus metus malesuada ante, at cursus massa turpis sit
+            amet
+          </p>
+        </div>
+      </div>
+      <div className="explore-section">
+        <div className="explore-details">
+          <h4>
+            <img src="src\assets\images\Frame (1).png" alt="...." />{" "}
+            <span></span> title h4
+          </h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
+            interdum augue sed tellus accumsan, nec congue quam lacinia. Duis
+            ornare congue ante a facilisis. Vestibulum sed magna in ligula
+            aliquam luctus non in ante. Nullam egestas, nunc at rutrum
+            tincidunt, purus metus malesuada ante, at cursus massa turpis sit
+            amet
+          </p>
+        </div>
+        <div className="explore-img pl">
+          <img src="src\assets\images\hero-image.png" alt="...." />
+          <div className="first-explore-card">
+            Explore <br /> Products
+          </div>
+        </div>
+      </div>
+      <div className="explore-section">
+        <div className="explore-img pr">
+          <img src="src\assets\images\hero-image.png" alt="...." />
+          <div className="second-explore-card">
+            Explore <br /> Customers
+          </div>
+        </div>
+        <div className="explore-details">
+          <h4>
+            <img src="src\assets\images\Frame (2).png" alt="...." />{" "}
+            <span></span> title h4
+          </h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
+            interdum augue sed tellus accumsan, nec congue quam lacinia. Duis
+            ornare congue ante a facilisis. Vestibulum sed magna in ligula
+            aliquam luctus non in ante. Nullam egestas, nunc at rutrum
+            tincidunt, purus metus malesuada ante, at cursus massa turpis sit
+            amet
+          </p>
+        </div>
+      </div>
+      <div className="more-section">
+        <button>Learn More</button>
+      </div>
+      <div className="one-liner-section">
+        <h4>h2 title one liner</h4>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras interdum
+          augue sed tellus accumsan, nec congue quam lacinia. Duis ornare congue
+          ante a facilisis. Vestibulum sed magna in ligula aliquam luctus non in
+          ante. Nullam egestas, nunc at rutrum tincid
+        </p>
+      </div>
+
+      <div className="branch-section">
+        <div className="first-branch">
+          <img
+            src="src\assets\images\Dynamic Shop Banner For Each 03.png"
+            alt="...."
+          />
+          <button>Branch 1</button>
+        </div>
+        <div className="second-branch">
+          <img
+            src="src\assets\images\Dynamic Shop Banner For Each 03.png"
+            alt="...."
+          />
+          <button>Branch 2</button>
+        </div>
+        <div className="third-branch">
+          <img
+            src="src\assets\images\Dynamic Shop Banner For Each 03.png"
+            alt="...."
+          />
+          <button>Branch 3</button>
+        </div>
+      </div>
+
+      <div className="more-section">
+        <button>See More</button>
+      </div>
+      <div className="notice-events-section">
+        <h4>h2 title one liner</h4>
+        <div className="notice-details">
+          <div className="preview-notice">
+            <img src="src\assets\images\hero-image.png" alt="...." />
+            <p>January 06, 2024</p>
+            <h4>Lorem ipsum dolor sit amet, consectetur</h4>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
+              interdum augue sed tellus accumsan, nec congue quam lacinia. Duis
+              ornare congue ante a facilisis. Vestibulum sed magna in ligula
+              aliquam luctus non in ante. Nullam egestas, nun
+            </p>
+          </div>
+          <div className="notice-events">
+            <div className="all-notice">
+              <img src="src\assets\images\hero-image.png" alt="...." />
+              <div className="all-notice-details">
+                <p>January 06, 2024</p>
+                <span>Lorem ipsum dolor sit amet, consectetur</span>
+              </div>
+            </div>
+            <div className="all-notice">
+              <img src="src\assets\images\hero-image.png" alt="...." />
+              <div className="all-notice-details">
+                <p>January 06, 2024</p>
+                <span>Lorem ipsum dolor sit amet, consectetur</span>
+              </div>
+            </div>
+            <div className="all-notice">
+              <img src="src\assets\images\hero-image.png" alt="...." />
+              <div className="all-notice-details">
+                <p>January 06, 2024</p>
+                <span>Lorem ipsum dolor sit amet, consectetur</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="more-section">
+        <button>View All Notice and Events</button>
+      </div>
+      <div className="second-events-section">
+        <h4>h2 title one liner</h4>
+        <div className="second-notice-details">
+          <div className="second-preview-notice">
+            <img src="src\assets\images\hero-image.png" alt="...." />
+            <div className="second-preview-date">
+              <h6>
+                OCT <br /> 31
+              </h6>
+              <div>
+                <h4>Lorem ipsum us </h4>
+                <p>January 06, 2024</p>
+              </div>
+            </div>
+          </div>
+          <div className="second-notice-events">
+            <div className="second-all-notice">
+              <h6>
+                OCT <br /> 31
+              </h6>
+              <div className="all-notice-details">
+                <span>Lorem ipsum dolor sit amet, consectetur</span>
+                <p>Lorem ipsum dolor sit amet, consectetur</p>
+              </div>
+            </div>
+            <div className="second-all-notice">
+              <h6>
+                OCT <br /> 31
+              </h6>
+              <div className="all-notice-details">
+                <span>Lorem ipsum dolor sit amet, consectetur</span>
+                <p>Lorem ipsum dolor sit amet, consectetur</p>
+              </div>
+            </div>
+            <div className="second-all-notice">
+              <h6>
+                OCT <br /> 31
+              </h6>
+              <div className="all-notice-details">
+                <span>Lorem ipsum dolor sit amet, consectetur</span>
+                <p>Lorem ipsum dolor sit amet, consectetur</p>
+              </div>
+            </div>
+
+            <button>View All Notice and Events</button>
+          </div>
+        </div>
+      </div>
+      <div className="one-liner-section">
+        <h4>h2 title one liner</h4>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras interdum
+          augue sed tellus accumsan, nec congue quam lacinia. Duis ornare congue
+          ante a facilisis. Vestibulum sed magna in ligula aliquam luctus non in
+          ante. Nullam egestas, nunc at rutrum tincid
+        </p>
+      </div>
+      <div className="multiple-cards-section">
+        <div>
+          <div className="single-card"></div>
+          <div className="single-card"></div>
+          <div className="single-card"></div>
+        </div>
+        <div className="second-cards-section">
+          <div className="single-card"></div>
+          <div className="single-card"></div>
+          <div className="single-card"></div>
+        </div>
+        <div>
+          <div className="single-card"></div>
+          <div className="single-card"></div>
+          <div className="single-card"></div>
+        </div>
+        <div className="fourth-cards-section">
+          <div className="single-card"></div>
+          <div className="single-card"></div>
+          <div className="single-card"></div>
+        </div>
+      </div>
+      <div className="learn-more-section">
+        <button>Learn More</button>
+      </div>
+      <div className="map-section">
+        <div>
+          <div className="address">
+            <div className="circle"></div>
+            <h4> Shop Name</h4>
+            <p>
+              {" "}
+              <img src="src\assets\images\frame (3).png" alt="...." /> Shop
+              location
+            </p>
+            <p>
+              <img src="src\assets\images\frame (4).png" alt="...." /> +XX XXX
+              XXX XXXX (Shop Admin){" "}
+            </p>
+            <p>
+              <img src="src\assets\images\frame (5).png" alt="...." />{" "}
+              info@shop.com
+            </p>
+          </div>
+        </div>
+        <div>
+          <img src="src\assets\images\Location Image.png" alt="...." />
+        </div>
+      </div>
+      <div className="footer-section">
+        &copy; 2024 Shop. All rights reserved
+      </div>
     </>
   );
 };
